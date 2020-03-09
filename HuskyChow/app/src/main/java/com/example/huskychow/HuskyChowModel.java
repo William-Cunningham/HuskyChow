f703e71348e522c66b8453129c87970c67681100
William Cunningham <cunningham.wil@husky.neu.edu>
2020-03-09 15:35:01 -0400

    Add files via upload
    added a remove restaurant method

--- HuskyChow/app/src/main/java/com/example/huskychow/HuskyChowModel.java
+++ HuskyChow/app/src/main/java/com/example/huskychow/HuskyChowModel.java
@@ -8,4 +8,21 @@ public class HuskyChowModel {
     //an ArrayList of Restaurant objects
     private ArrayList<Restaurant> restaurantList;
 
+    //TODO need to know the information needed to construct a restaurant (from API)
+    public void addRestaurant() {
+
+    }
+
+    //removes the first restaurant in the list that matches by name
+    //compares after normalizing capitalization
+    public void removeRestaurant(String name) {
+        for (Restaurant restaurant : this.restaurantList) {
+            if (restaurant.getName().toLowerCase().equals(name.toLowerCase())) {
+                //mutates the list of restaurants
+                this.restaurantList.remove(restaurant);
+            }
+        }
+    }
+
+
 }
